06277ca0287dd1fcf88a72445089f89ae5e372b7
mrsafadi2 <mrsafadi2@gmail.com>
2022-12-21 07:23:33 +0200

    Initial Commit

---
+++ GeneralHudTypesSwiftui/GeneralHudTypesSwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeneralHudTypesSwiftuiApp.swift
+//  GeneralHudTypesSwiftui
+//
+//  Created by Mohammed Safadi Macbook Pro on 21/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct GeneralHudTypesSwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
